71f29def2e1f6df35015d63df34a713a86935b3b
Umar Baloch <umarbaloch262@gmail.com>
2023-12-29 17:28:37 +0500

    added templates, and get requests

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
+
+todos = ['default']
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Wrold!!</p>"
+    return render_template('index.html')
+
+@app.route("/addTodo",methods=["GET"])
+def addTodo():
+    todos.append(2)
+
+
+@app.route("/deleteTodo",methods=["DELETE"])
+def deleteTodo():
+    return "deleted"
 
 app.run()
